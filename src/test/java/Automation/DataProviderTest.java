4f4ffb976ceee6a0c18b6700e43197c7bc573a9a
wahidursqa <wahidursqa@gmail.com>
2023-07-02 13:29:56 +0600

    Data Provider in Testng

---
+++ src/test/java/Automation/DataProviderTest.java
@@ -0,0 +1,52 @@
+package Automation;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class DataProviderTest {
+	@Test(dataProvider = "loginTestData")
+	public void Login(String userName, String password) throws Exception {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		
+		driver.get("https://opensource.demo.orangehrmlive.com/");
+		driver.findElement(By.id("txtUsername")).sendKeys(userName);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.id("btnLogin")).click();
+		Assert.assertTrue(driver.findElement(By.id("welcome")).isDisplayed());
+		Thread.sleep(5000);
+		driver.quit();			
+		
+		
+	}
+	
+	@DataProvider(name="loginTestData")
+	
+	public Object [][] LoginData() {
+		Object [][] data = new Object [4][2];
+		
+		data [0][0] ="Admin";
+		data [0][1] ="admin123";
+		
+		data [1][0] ="Admin";
+		data [1][1] ="test123";
+		
+		data [2][0] ="Admin568";
+		data [2][1] ="admin123";
+		
+		data [3][0] ="Admin568";
+		data [3][1] ="test123";
+		
+		return data;
+	}
+	
+}
